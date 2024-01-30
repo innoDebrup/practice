06442c73f6a82bd7e39732cd44d46030debdabfc
innoDebrup <debrup.pramanik@innoraft.com>
2024-01-30 16:16:34 +0530

    added responsive through media queries

--- htmlCSS/a1/a1.js
+++ htmlCSS/a1/a1.js
@@ -7,4 +7,29 @@ $(document).ready(function(){
       $(".topHeader").slideDown(500);
     }
   })
+  $("#ham").click(function(){
+    $("#navLinks").slideDown(500);
+    $("#search").slideDown(500);
+    $("#cross").show();
+    $(".right").slideDown(500);
+    $(this).hide();
+  })
+  $("#cross").click(function(){
+    $("#navLinks").slideUp(500);
+    $("#search").slideUp(500);
+    $("#ham").show();
+    $(".right").slideUp(500);
+    $(this).hide();
+  })
+
+  $("#cdown").click(function(){
+    $(".topHeader").animate({height: "400px"},700);
+    $("#cup").show();
+    $(this).hide();
+  })
+  $("#cup").click(function(){
+    $(".topHeader").animate({height: "140px"},700);
+    $("#cdown").show();
+    $(this).hide();
+  })
 })
